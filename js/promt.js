10dbbea9cccddeb567b63b967d0d44dac5a7b29f
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-18 11:18:04 +0200

    Update promt.js

--- js/promt.js
+++ js/promt.js
@@ -1,17 +1,23 @@
 // prompt
+/*
 var name = prompt("What's your name?");
 console.log("Hello " + name);
+*/
 
 // confirm
+/*
 var likesCats = confirm("Do you likes cats?");
 if (likesCats) {
   console.log("You are cool cat!");
 } else {
   console.log("Yeah, that's fine. You're still cool!");
 }
+*/
 
 // alert
+/*
 alert("JavaScript is awesome!");
+*/
 
 // pseudo-code
 /*
