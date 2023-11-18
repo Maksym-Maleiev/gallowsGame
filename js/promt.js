1775ca104e2f9f42bf8a0cac4bb44355bd53f680
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-18 10:15:30 +0200

    Update promt.js

--- js/promt.js
+++ js/promt.js
@@ -12,3 +12,27 @@ if (likesCats) {
 
 // alert
 alert("JavaScript is awesome!");
+
+// pick
+/*
+Pick a random word (Виберіть навмання слово)
+
+While the word has not been guessed (Поки слово не вгадали) {
+  Show the player their current progress (Показати гравцеві їхній поточний прогрес)
+  Get a guess from the player (Отримайте припущення від гравця)
+
+  If the player wants to quit the game {
+    Quit the game
+  }
+  Else if the guess is not a single letter {
+    Tell the player to pick a single letter
+  }
+  Else {
+    If the guess is in the word {
+      Update the player's progress with the guess
+    }
+  }
+}
+
+Congratulate the player on guessing the word
+*/
