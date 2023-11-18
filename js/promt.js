7c408d6d3a419dbaa8bffdef6074780602ba01de
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-18 09:51:13 +0200

    Update promt.js

--- js/promt.js
+++ js/promt.js
@@ -3,3 +3,9 @@ var name = prompt("What's your name?");
 console.log("Hello " + name);
 
 // confirm
+var likesCats = confirm("Do you likes cats?");
+if (likesCats) {
+  console.log("You are cool cat!");
+} else {
+  console.log("Yeah, that's fine. You're still cool!");
+}
