11a515eba674e4747efe2c700a64303d2e0fbfc5
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-18 10:50:08 +0200

    Update promt.js

--- js/promt.js
+++ js/promt.js
@@ -1,17 +1,17 @@
 // prompt
-var name = prompt("What's your name?");
-console.log("Hello " + name);
+// var name = prompt("What's your name?");
+// console.log("Hello " + name);
 
 // confirm
-var likesCats = confirm("Do you likes cats?");
-if (likesCats) {
-  console.log("You are cool cat!");
-} else {
-  console.log("Yeah, that's fine. You're still cool!");
-}
+// var likesCats = confirm("Do you likes cats?");
+// if (likesCats) {
+//   console.log("You are cool cat!");
+// } else {
+//   console.log("Yeah, that's fine. You're still cool!");
+// }
 
 // alert
-alert("JavaScript is awesome!");
+// alert("JavaScript is awesome!");
 
 // pseudo-code
 /*
@@ -36,3 +36,5 @@ While the word has not been guessed (Поки слово не вгадали) {
 
 Congratulate the player on guessing the word (Привітайте гравця з вгадуванням слова)
 */
+
+// #1: Choose random word
