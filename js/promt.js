b0aa499b25a8293b906fe6629f9d019b522afd28
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-18 10:51:31 +0200

    Update promt.js

--- js/promt.js
+++ js/promt.js
@@ -1,17 +1,17 @@
 // prompt
-// var name = prompt("What's your name?");
-// console.log("Hello " + name);
+var name = prompt("What's your name?");
+console.log("Hello " + name);
 
 // confirm
-// var likesCats = confirm("Do you likes cats?");
-// if (likesCats) {
-//   console.log("You are cool cat!");
-// } else {
-//   console.log("Yeah, that's fine. You're still cool!");
-// }
+var likesCats = confirm("Do you likes cats?");
+if (likesCats) {
+  console.log("You are cool cat!");
+} else {
+  console.log("Yeah, that's fine. You're still cool!");
+}
 
 // alert
-// alert("JavaScript is awesome!");
+alert("JavaScript is awesome!");
 
 // pseudo-code
 /*
@@ -38,3 +38,4 @@ Congratulate the player on guessing the word (Привітайте гравця 
 */
 
 // #1: Choose random word
+var words = ["javascript", "monkey", "amazing", "pancake"];
